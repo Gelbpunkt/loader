c2d3cb2a4783e8f7f098050230aee4cd98cffb34
Aelin Reidel <aelin@travitia.xyz>
2026-05-15 04:21:42 +0200

    refactor(aarch64,riscv64): unify kernel image detection
    This unifies the kernel image detection for both aarch64 and riscv64.
    riscv64 was incorrectly using the length of the module reg as the size
    of the kernel image, which was always 0, which is fixed by using the
    approach taken by the aarch64 code to parse the ELF header. On the other
    hand, aarch64 now falls back to linux,initrd-start and linux,initrd-end
    for detecting the kernel image thanks to these changes, which is
    necessary for cloud-hypervisor at the moment.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,8 @@
 #![allow(clippy::missing_safety_doc)]
 
 use ::log::info;
+#[cfg(any(target_arch = "aarch64", target_arch = "riscv64"))]
+use goblin::elf::header::header64::{EI_DATA, ELFDATA2LSB, ELFMAG, Header, SELFMAG};
 use hermit_entry::boot_info::{BootInfo, RawBootInfo};
 
 #[macro_use]
@@ -36,6 +38,55 @@ impl BootInfoExt for BootInfo {
 		let raw_boot_info = RAW_BOOT_INFO.take().unwrap();
 
 		raw_boot_info.insert(RawBootInfo::from(self))
+	}
+}
+
+#[cfg(any(target_arch = "aarch64", target_arch = "riscv64"))]
+trait FdtExt {
+	fn find_kernel(&self) -> Option<&'static [u8]>;
+}
+
+#[cfg(any(target_arch = "aarch64", target_arch = "riscv64"))]
+impl FdtExt for fdt::Fdt<'_> {
+	fn find_kernel(&self) -> Option<&'static [u8]> {
+		let chosen = self.find_node("/chosen").unwrap();
+
+		// The reg size of the module nodes is always 0, so we cannot trust them and
+		// instead need to parse the ELF header
+		let find_module_start = || {
+			let module = chosen
+				.children()
+				.find(|node| node.name.starts_with("module@"))?;
+			let start_ptr = module.reg().unwrap().next().unwrap().starting_address;
+			let header = unsafe { &*core::mem::transmute::<*const u8, *const Header>(start_ptr) };
+
+			if header.e_ident[0..SELFMAG] != ELFMAG[..] {
+				return None;
+			}
+
+			let len = if header.e_ident[EI_DATA] == ELFDATA2LSB {
+				u64::from_le(header.e_shoff)
+					+ (u16::from_le(header.e_shentsize) as u64
+						* u16::from_le(header.e_shnum) as u64)
+			} else {
+				u64::from_be(header.e_shoff)
+					+ (u16::from_be(header.e_shentsize) as u64
+						* u16::from_be(header.e_shnum) as u64)
+			};
+
+			Some(unsafe { core::slice::from_raw_parts(start_ptr, len.try_into().unwrap()) })
+		};
+
+		let find_linux_initrd = || {
+			let start = chosen.property("linux,initrd-start")?.as_usize()?;
+			let end = chosen.property("linux,initrd-end")?.as_usize()?;
+			let start_ptr = core::ptr::with_exposed_provenance::<u8>(start);
+			let end_ptr = core::ptr::with_exposed_provenance::<u8>(end);
+			let len = unsafe { end_ptr.offset_from(start_ptr).try_into().unwrap() };
+			Some(unsafe { core::slice::from_raw_parts(start_ptr, len) })
+		};
+
+		find_module_start().or_else(find_linux_initrd)
 	}
 }
 
